ca35213a0b08ff98e41304a06f270c1ec542db22
Timo Goebel <mail@timogoebel.name>
2017-12-28 16:53:53 +0100

    code cleanups

--- lib/smart_proxy_realm_ad/configuration_loader.rb
+++ lib/smart_proxy_realm_ad/configuration_loader.rb
@@ -1,20 +1,20 @@
 module Proxy::AdRealm
-    class ConfigurationLoader
-        def load_classes
-            require 'smart_proxy_realm_ad/provider'
-        end
-
-        def load_dependency_injection_wirings(container_instance, settings)
-            container_instance.dependency:realm_provider_impl,
-                lambda {
-                    ::Proxy::AdRealm::Provider.new(
-                        settings[:realm],
-                        settings[:keytab_path],
-                        settings[:principal],
-                        settings[:domain_controller],
-                        settings[:ou]
-                    )
-                }
-        end
+  class ConfigurationLoader
+    def load_classes
+      require 'smart_proxy_realm_ad/provider'
     end
+
+    def load_dependency_injection_wirings(container_instance, settings)
+      container_instance.dependency :realm_provider_impl,
+                                    lambda {
+                                      ::Proxy::AdRealm::Provider.new(
+                                        settings[:realm],
+                                        settings[:keytab_path],
+                                        settings[:principal],
+                                        settings[:domain_controller],
+                                        settings[:ou]
+                                      )
+                                    }
+    end
+  end
 end
--- lib/smart_proxy_realm_ad/plugin.rb
+++ lib/smart_proxy_realm_ad/plugin.rb
@@ -1,12 +1,12 @@
 require 'smart_proxy_realm_ad/version'
 
 module Proxy::AdRealm
-    class Plugin < Proxy::Provider
-        load_classes ::Proxy::AdRealm::ConfigurationLoader
-        load_dependency_injection_wirings ::Proxy::AdRealm::ConfigurationLoader
+  class Plugin < Proxy::Provider
+    load_classes ::Proxy::AdRealm::ConfigurationLoader
+    load_dependency_injection_wirings ::Proxy::AdRealm::ConfigurationLoader
 
-        validate_presence :realm, :keytab_path, :principal, :domain_controller
+    validate_presence :realm, :keytab_path, :principal, :domain_controller
 
-        plugin :realm_ad, ::Proxy::AdRealm::VERSION
-    end
+    plugin :realm_ad, ::Proxy::AdRealm::VERSION
+  end
 end
